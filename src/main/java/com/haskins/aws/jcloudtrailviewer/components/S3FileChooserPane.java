464478112b75eda926924835e76f8a4ce6ea3bb3
Mark <markhaskins@mac.com>
2014-06-08 18:16:08 +0100

    Added Bread Crumb to top of S3 File Chooser

--- src/main/java/com/haskins/aws/jcloudtrailviewer/components/S3FileChooserPane.java
+++ src/main/java/com/haskins/aws/jcloudtrailviewer/components/S3FileChooserPane.java
@@ -13,12 +13,15 @@ import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MultipleSelectionModel;
+import javafx.scene.control.TreeItem;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import org.controlsfx.control.BreadCrumbBar;
+import org.controlsfx.control.BreadCrumbBar.BreadCrumbActionEvent;
 
 /**
  *
@@ -26,8 +29,10 @@ import javafx.scene.layout.Pane;
  */
 public class S3FileChooserPane extends Pane {
     
-    private ListView<String> list = new ListView<>();
     private ObservableList<String> items = FXCollections.observableArrayList ();
+    
+    private ListView<String> list = new ListView<>();
+    private BreadCrumbBar breadCrumb = new BreadCrumbBar();       
     
     private String prefix = "";
     
@@ -51,50 +56,7 @@ public class S3FileChooserPane extends Pane {
         
         return selectedFiles;
     }
-    
-    public void reloadContents() {
-               
-        List<String> tmp = new ArrayList<>();
-        this.items.setAll(tmp);
-                
-        String bucketName = PropertiesSingleton.getInstance().getProperty("Bucket");
         
-        ListObjectsRequest listObjectsRequest = new ListObjectsRequest();
-        listObjectsRequest.setBucketName(bucketName);
-        listObjectsRequest.setPrefix(prefix);
-        listObjectsRequest.setDelimiter("/");
-        
-		AWSCredentials credentials = 
-            new BasicAWSCredentials(
-                PropertiesSingleton.getInstance().getProperty("Key"), 
-                PropertiesSingleton.getInstance().getProperty("Secret")
-            );
-        
-        AmazonS3 s3Client = new AmazonS3Client(credentials);
-        
-        
-        ObjectListing objectListing = s3Client.listObjects(listObjectsRequest);
-        
-        // these are directories
-        List<String> directories = objectListing.getCommonPrefixes();
-        
-        for (String directory : directories) {
-            
-            tmp.add( stripPrefix( directory) );
-            
-        }
-        
-        // these are files
-        List<S3ObjectSummary> files = objectListing.getObjectSummaries();
-        
-        for (final S3ObjectSummary objectSummary : files) {
-        
-            tmp.add( stripPrefix( objectSummary.getKey() ) );
-        }
-        
-        this.items.setAll(tmp);
-    }
-    
     ////////////////////////////////////////////////////////////////////////////
     ///// private methods
     ////////////////////////////////////////////////////////////////////////////
@@ -105,8 +67,15 @@ public class S3FileChooserPane extends Pane {
         
         BorderPane root = new BorderPane();
         
-        // Drop down
-        ChoiceBox hierarchy = new ChoiceBox();        
+        // BreadCrumb
+        breadCrumb.setPrefWidth(620);
+        breadCrumb.setSelectedCrumb(new TreeItem("/"));
+        breadCrumb.setOnCrumbAction(new EventHandler<BreadCrumbBar.BreadCrumbActionEvent>() {
+            @Override
+            public void handle(BreadCrumbActionEvent event) {                
+                updateBreadCrumb(event.getSelectedCrumb());
+            }
+        });
         
         // files area
         list.setItems(items);
@@ -123,11 +92,11 @@ public class S3FileChooserPane extends Pane {
         });
                
         
-        root.setTop(hierarchy);
+        root.setTop(breadCrumb);
         root.setCenter(list);
         
         list.setPrefHeight( 460 );
-        list.setPrefWidth( 610 );
+        list.setPrefWidth( 620 );
         
         this.getChildren().add(root);
     }
@@ -168,5 +137,71 @@ public class S3FileChooserPane extends Pane {
         } 
         
         return stripped;
+    }
+    
+    private void reloadContents() {
+               
+        List<String> tmp = new ArrayList<>();
+        this.items.setAll(tmp);
+                
+        String bucketName = PropertiesSingleton.getInstance().getProperty("Bucket");
+        
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest();
+        listObjectsRequest.setBucketName(bucketName);
+        listObjectsRequest.setPrefix(prefix);
+        listObjectsRequest.setDelimiter("/");
+        
+		AWSCredentials credentials = 
+            new BasicAWSCredentials(
+                PropertiesSingleton.getInstance().getProperty("Key"), 
+                PropertiesSingleton.getInstance().getProperty("Secret")
+            );
+        
+        AmazonS3 s3Client = new AmazonS3Client(credentials);
+        
+        
+        ObjectListing objectListing = s3Client.listObjects(listObjectsRequest);
+        
+        // these are directories
+        List<String> directories = objectListing.getCommonPrefixes();
+        
+        for (String directory : directories) {
+            
+            tmp.add( stripPrefix( directory) );
+        }
+        
+        // these are files
+        List<S3ObjectSummary> files = objectListing.getObjectSummaries();
+        
+        for (final S3ObjectSummary objectSummary : files) {
+        
+            tmp.add( stripPrefix( objectSummary.getKey() ) );
+        }
+        
+        updateBreadCrumb();
+        
+        this.items.setAll(tmp);
+    }
+    
+    private void updateBreadCrumb(TreeItem selected) {
+        
+        String newPrefix = selected.getValue().toString() + "/";
+
+        while (selected.getParent() != null) {
+
+            selected = selected.getParent();
+            newPrefix = selected.getValue().toString() + "/" + newPrefix;
+        }
+
+        System.out.println(newPrefix);
+
+        this.prefix = newPrefix;
+        
+        reloadContents();
+    }
+    
+    private void updateBreadCrumb() {
+                
+        breadCrumb.setSelectedCrumb( BreadCrumbBar.buildTreeModel( this.prefix.split("/")) );
     }
 }
