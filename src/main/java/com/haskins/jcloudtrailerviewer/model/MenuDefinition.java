d2c4e8e1a067fe1f4cce7d6146d85216ad29a15f
Mark <markhaskins@mac.com>
2015-03-14 21:33:22 +0000

    New Models for handling user defined menus

---
+++ src/main/java/com/haskins/jcloudtrailerviewer/model/MenuDefinition.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2015 Mark P. Haskins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.haskins.jcloudtrailerviewer.model;
+
+import java.util.List;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+/**
+ *
+ * @author mark.haskins
+ */
+public class MenuDefinition {
+    
+    @JsonProperty("Menu")
+    private String menu;
+    
+    @JsonProperty("SubMenu")
+    private String subMenu;
+    
+    @JsonProperty("Name")
+    private String name;
+    
+    @JsonProperty("Panel")
+    private String panel;
+    
+    @JsonProperty("Actions")
+    private List<String> actions;
+
+    /**
+     * @return the menu
+     */
+    public String getMenu() {
+        return menu;
+    }
+
+    /**
+     * @param menu the menu to set
+     */
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    /**
+     * @return the subMenu
+     */
+    public String getSubMenu() {
+        return subMenu;
+    }
+
+    /**
+     * @param subMenu the subMenu to set
+     */
+    public void setSubMenu(String subMenu) {
+        this.subMenu = subMenu;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the panel
+     */
+    public String getPanel() {
+        return panel;
+    }
+
+    /**
+     * @param panel the panel to set
+     */
+    public void setPanel(String panel) {
+        this.panel = panel;
+    }
+
+    /**
+     * @return the actions
+     */
+    public List<String> getActions() {
+        return actions;
+    }
+
+    /**
+     * @param actions the actions to set
+     */
+    public void setActions(List<String> actions) {
+        this.actions = actions;
+    }
+}
