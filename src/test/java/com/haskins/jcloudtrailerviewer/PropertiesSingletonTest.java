3a31cb5bdfd971057b1052cdceb48f5ee503a76d
Mark <markhaskins@mac.com>
2015-03-28 16:08:30 +0000

    created unit test for PropertiesSingleton

---
+++ src/test/java/com/haskins/jcloudtrailerviewer/PropertiesSingletonTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.haskins.jcloudtrailerviewer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mark
+ */
+public class PropertiesSingletonTest {
+    
+    public PropertiesSingletonTest() {
+    }
+
+    /**
+     * Test of getInstance method, of class PropertiesSingleton.
+     */
+    @Test
+    public void testGetInstance() {        
+        assertNotNull(PropertiesSingleton.getInstance());
+    }
+
+    /**
+     * Test of setProperty method, of class PropertiesSingleton.
+     */
+    @Test
+    public void testSetProperty() {
+        
+        String key = "TestKey";
+        String value = "TestValue";
+        PropertiesSingleton.getInstance().setProperty(key, value);
+        
+        String newValue = PropertiesSingleton.getInstance().getProperty(key);
+        
+        assertEquals(value, newValue);
+    }
+
+    /**
+     * Test of getProperty method, of class PropertiesSingleton.
+     */
+    @Test
+    public void testGetProperty() {
+        
+        String key = "TestKey";
+        String value = "TestValue";
+        PropertiesSingleton.getInstance().setProperty(key, value);
+        
+        String newValue = PropertiesSingleton.getInstance().getProperty(key);
+        
+        assertEquals(value, newValue);
+    }
+
+    /**
+     * Test of configLoaded method, of class PropertiesSingleton.
+     */
+    @Test
+    public void testConfigLoaded() {
+        
+        PropertiesSingleton.getInstance();
+        boolean expResult = true;
+        boolean result = PropertiesSingleton.getInstance().configLoaded();
+        
+        assertEquals(expResult, result);
+
+    }
+    
+}
