370cd57ff8f5b2f602fea2576a32a2192b2076b9
Mark <markhaskins@mac.com>
2015-08-06 21:13:38 +0100

    Preperation for security feature

--- src/main/java/com/haskins/cloudtrailviewer/feature/security/SecurityFeature.java
+++ src/main/java/com/haskins/cloudtrailviewer/feature/security/SecurityFeature.java
@@ -1,8 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*    
+CloudTrail Viewer, is a Java desktop application for reading AWS CloudTrail logs
+files.
+
+Copyright (C) 2015  Mark P. Haskins
+
+This program is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.haskins.cloudtrailviewer.feature.security;
 
 import com.haskins.cloudtrailviewer.core.EventDatabaseListener;
@@ -66,4 +79,8 @@ public class SecurityFeature extends JPanel implements Feature, EventDatabaseLis
     public void eventAdded(Event event) {
                  
     }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    ///// private methods
+    ////////////////////////////////////////////////////////////////////////////
 }
