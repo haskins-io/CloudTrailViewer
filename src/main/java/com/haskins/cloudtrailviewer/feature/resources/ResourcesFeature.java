7c2b068d7cc61ecfe986bd5460eaeaf05e5f6e3a
mark haskins <mark.haskins@ocado.com>
2015-08-07 12:58:06 +0100

    Rename ServiceFeature to ResourceFeature

--- src/main/java/com/haskins/cloudtrailviewer/feature/resources/ResourcesFeature.java
+++ src/main/java/com/haskins/cloudtrailviewer/feature/resources/ResourcesFeature.java
@@ -16,12 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-package com.haskins.cloudtrailviewer.feature.service;
+package com.haskins.cloudtrailviewer.feature.resources;
 
 /**
  *
  * @author mark.haskins
  */
-public class ServiceFeature {
+public class ResourcesFeature {
     
 }
