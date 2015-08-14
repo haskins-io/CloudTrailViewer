81223e576de812f801cbb740d07fff81888d33aa
mark haskins <mark.haskins@ocado.com>
2015-08-14 13:19:04 +0100

    removed stray semi-colon

--- src/main/java/com/haskins/cloudtrailviewer/CloudTrailViewer.java
+++ src/main/java/com/haskins/cloudtrailviewer/CloudTrailViewer.java
@@ -34,7 +34,7 @@ public class CloudTrailViewer extends JFrame {
     public static CloudTrailViewerApplication frame;
     
     private static void createAndShowGUI() {
-;        
+                
         JFrame.setDefaultLookAndFeelDecorated(true);
         
         Menu menu = new Menu();    
