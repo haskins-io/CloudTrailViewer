f985c5af6081c02ad93c44b534bd7ab573a61ff6
mark haskins <mark.haskins@ocado.com>
2016-01-22 16:53:36 +0000

    defined a new Dimension so all extenders can implement to be consistent

--- src/main/java/com/haskins/cloudtrailviewer/model/filter/AbstractFilter.java
+++ src/main/java/com/haskins/cloudtrailviewer/model/filter/AbstractFilter.java
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package com.haskins.cloudtrailviewer.model.filter;
 
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +27,8 @@ import java.util.List;
  * @author mark
  */
 public abstract class AbstractFilter implements Filter {
+    
+    protected Dimension defaultSize = new Dimension(345,30);
     
     private final List<FilterListener> listeners = new ArrayList<>();
     
