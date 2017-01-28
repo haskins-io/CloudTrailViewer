a4730254b2a9bd9c7bab66d94778ee08938e86e9
mark haskins <markhaskins@mac.com>
2017-01-28 15:06:23 +0000

    added function to return file extension

--- src/main/java/io/haskins/java/cloudtrailviewer/utils/FileUtils.java
+++ src/main/java/io/haskins/java/cloudtrailviewer/utils/FileUtils.java
@@ -80,4 +80,15 @@ public class FileUtils {
         return filename.replaceFirst("[.][^.]+$", "");
     }
 
+    public static String getFileExtension(File file) {
+
+        String fileName = file.getName();
+
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        }
+
+        else return "";
+    }
+
 }
