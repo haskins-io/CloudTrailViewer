0dec4f35e931c7cb4acd42e6348e59336257a53a
mark haskins <markhaskins@mac.com>
2017-02-05 20:35:17 +0000

    started on getting help working

--- src/main/java/io/haskins/java/cloudtrailviewer/controller/menu/HelpMenuController.java
+++ src/main/java/io/haskins/java/cloudtrailviewer/controller/menu/HelpMenuController.java
@@ -18,7 +18,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package io.haskins.java.cloudtrailviewer.controller.menu;
 
+import javafx.fxml.FXML;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Controller that handles the Help menu
@@ -26,5 +34,33 @@ import org.springframework.stereotype.Component;
  * Created by markhaskins on 06/01/2017.
  */
 @Component
-class HelpMenuController {
+public class HelpMenuController {
+
+    @FXML
+    private void showUserGuide() {
+
+        StringBuilder result = new StringBuilder();
+
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        InputStreamReader io = new InputStreamReader(classLoader.getResourceAsStream("docs/UserGuide.md"));
+
+        try( BufferedReader br = new BufferedReader(io) ) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                result.append(line).append("\n");
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(result.toString());
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        String userGuide = renderer.render(document);
+
+//        System.out.print(userGuide);
+
+    }
 }
