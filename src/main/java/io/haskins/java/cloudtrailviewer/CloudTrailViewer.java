1ecece85e1c977e424f598625ad1ca31c9e31b59
mark haskins <markhaskins@mac.com>
2017-02-05 12:47:56 +0000

    added basic splash screen and move spring appliction initialisation into a task thread

--- src/main/java/io/haskins/java/cloudtrailviewer/CloudTrailViewer.java
+++ src/main/java/io/haskins/java/cloudtrailviewer/CloudTrailViewer.java
@@ -18,52 +18,131 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package io.haskins.java.cloudtrailviewer;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.URL;
+
 /**
  *
  * Entry point to application.
+ *
+ * The splash page logic is based on an example found here : https://gist.github.com/jewelsea/2305098
  *
  * Created by markhaskins on 03/01/2017.
  */
 @SpringBootApplication
 public class CloudTrailViewer extends Application {
 
-    private static final ConfigurableApplicationContext springContext = SpringApplication.run(CloudTrailViewer.class);
+    private Pane splashLayout;
+
+    private static final int SPLASH_WIDTH = 676;
+    private static final int SPLASH_HEIGHT = 227;
+
+    private ConfigurableApplicationContext springContext;
 
     private BorderPane rootPane;
 
     public static void main(final String[] args) {
         Application.launch(args);
     }
+
     @Override
     public void init() throws Exception {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
-        fxmlLoader.setControllerFactory(springContext::getBean);
-        rootPane = fxmlLoader.load();
+        Image image = new Image("/images/splash.png");
+        ImageView splash = new ImageView(image);
+
+        splashLayout = new VBox();
+        splashLayout.getChildren().addAll(splash);
+
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        final Task<Void> friendTask = new Task<Void>() {
+            @Override
+            protected Void call() throws InterruptedException, IOException {
+
+                springContext = SpringApplication.run(CloudTrailViewer.class);
+
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+                fxmlLoader.setControllerFactory(springContext::getBean);
+                rootPane = fxmlLoader.load();
+
+                return null;
+            }
+        };
+
+        showSplash(stage, friendTask, this::showMainStage);
+        new Thread(friendTask).start();
+    }
+
+    private void showMainStage() {
+
         Scene scene = new Scene(rootPane);
         scene.getStylesheets().add(getClass().getResource("/style/cloudtrailviewer.css").toExternalForm());
 
-        stage.setScene(scene);
-        stage.setTitle("CloudTrail Viewer");
-        stage.show();
+        Stage mainStage = new Stage(StageStyle.DECORATED);
+        mainStage.setScene(scene);
+        mainStage.setTitle("CloudTrail Viewer");
+        mainStage.show();
+    }
+
+    private void showSplash(final Stage initStage, Task<?> task, InitCompletionHandler initCompletionHandler) {
+
+        task.stateProperty().addListener((observableValue, oldState, newState) -> {
+
+            if (newState == Worker.State.SUCCEEDED) {
+
+                initStage.toFront();
+                FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1.2), splashLayout);
+                fadeSplash.setFromValue(1.0);
+                fadeSplash.setToValue(0.0);
+                fadeSplash.setOnFinished(actionEvent -> initStage.hide());
+                fadeSplash.play();
+
+                initCompletionHandler.complete();
+            }
+        });
+
+        Scene splashScene = new Scene(splashLayout, Color.TRANSPARENT);
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        initStage.setScene(splashScene);
+        initStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        initStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        initStage.initStyle(StageStyle.TRANSPARENT);
+        initStage.setAlwaysOnTop(true);
+        initStage.show();
     }
 
     @Override
     public void stop() throws Exception {
         springContext.close();
+    }
+
+    public interface InitCompletionHandler {
+        void complete();
     }
 }
