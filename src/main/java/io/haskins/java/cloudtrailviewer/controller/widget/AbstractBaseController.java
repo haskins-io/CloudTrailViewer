45fe3bcc37bf304bba0985282124f52287407448
mark haskins <markhaskins@mac.com>
2017-02-05 20:34:14 +0000

    tidied up imports

--- src/main/java/io/haskins/java/cloudtrailviewer/controller/widget/AbstractBaseController.java
+++ src/main/java/io/haskins/java/cloudtrailviewer/controller/widget/AbstractBaseController.java
@@ -35,7 +35,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 import java.util.*;
